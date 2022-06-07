47649b4d43e2db53a04a5e506bd0b73271c7e3c0
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-06-07 06:18:26 +0600

    dashboard section added initially

--- src/App.js
+++ src/App.js
@@ -8,12 +8,15 @@ import Services from './pages/Services';
 import About from './pages/About';
 import PrivateRoute from "./authentication/PrivateRoute";
 import { privateRoutes } from '../src/routes/privateRoutes';
+import AdminRoute from "./authentication/AdminRoute";
+import Dashboard from "./pages/Dashboard/Dashboard";
 function App() {
   useEffect(() => {
     AOS.init();
   }, [])
   return (
     <Navbar>
+
       <Routes>
         {
           publicRoute.map(({ path, Component }, index) => (<Route key={index} path={path} element={<Component />} />))
@@ -25,7 +28,12 @@ function App() {
           }
         </Route>
 
+        <Route element={<AdminRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+
       </Routes>
+
     </Navbar>
   );
 }
